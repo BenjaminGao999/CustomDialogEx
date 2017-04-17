c92d621bf3bef8b3dfe28ae7053fd54468ae92e4
littlelittle <1986118324@qq.com>
2017-04-17 10:10:41 +0800

    Window和WindowManager是啥？以及2者关系。

--- CustomDialogExApp/src/main/java/com/gaos/customdialogex/MainActivity.java
+++ CustomDialogExApp/src/main/java/com/gaos/customdialogex/MainActivity.java
@@ -1,11 +1,16 @@
 package com.gaos.customdialogex;
 
 import android.support.design.widget.BottomSheetDialog;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialog;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +23,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initView();
 
         initListener();
+
+        windowManagerEX();
+
     }
 
 
@@ -40,10 +48,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    //BottomSheetDialog兼容到api level 16
+    //BottomSheetDialog兼容到api level 15
     private void showBottomSheetDialog() {
         BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
         bottomSheetDialog.setContentView(R.layout.layout_bottomsheetdialog);
         bottomSheetDialog.show();
+    }
+
+    //Window|WindowManager
+    private void windowManagerEX() {
+        //获取WindowManager，方法①
+        WindowManager windowManager = (WindowManager) getApplicationContext().getSystemService(WINDOW_SERVICE);
+        //获取WindowManager，方法②
+        WindowManager windowManager1 = getWindowManager();
+        //获取WindowManager，方式③
+        WindowManager windowManager2 = getWindow().getWindowManager();
+
+        //WindowManager能干点什么呢？
+        Display defaultDisplay = windowManager.getDefaultDisplay();
+
+//        ImageView imageView = new ImageView(this);
+//        imageView.setImageResource(R.drawable.soilder);
+//        windowManager.addView(imageView, new WindowManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
     }
 }
